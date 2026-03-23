0dd24910cb1d4375d55122822e4749fc76c18336
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-23 14:00:26 +0100

    Update lab_matrice.c

--- lab_matrice.c
+++ lab_matrice.c
@@ -3,44 +3,38 @@
 #include <stdbool.h>
 #include <assert.h>
 
-//idées : 
-
-typedef int** lab; //matrice classique
+/*idées : 
+ 
+*/
+typedef int* lab; //matrice classique
+//down = ind + q
 
 lab create(int p, int q){
-    lab labyrinthe = malloc(sizeof(int*)*q);
-    for(int i=0;i<len;i++){
-        lab[i] = calloc(sizeof(int)*p)
-    }
-    return ;    
+    lab labyrinthe = calloc(p,sizeof(int*));
+    return labyrinthe;    
 }
 
-bool is_real(lab ){
+lab init(int p, int q){
+
+}
+
+int* voisin(lab laby, int p, int q, int u){
+    int nb_v = 0;
+    if(u-1 >= 0) nb_v++;
+    if(u+1 <= q) nb_v++;
+    if(u+q <= p) nb_v++;
+    if(u-q >= 0) nb_v++;
+    int* v = malloc(sizeof(int)*nb_v);
+    int i = 0;
+    if(u-1 >= 0) {v[i] = u-1; i++;}
+    if(u+1 <= q) {v[i] = u+1; i++; }
+    if(u+q <= p) { ; i++; }
+    if(u-q >= 0) { ; i++; }
+}
+
+bool is_real(lab laby, int start){
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
