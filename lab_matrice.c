3974c5c6ec12a0430977bfc0f1fec885e1ce3a94
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-23 13:23:19 +0100

    Create lab_matrice.c

---
+++ lab_matrice.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+//idées : 
+
+typedef int** lab; //matrice classique
+
+lab create(int p, int q){
+    lab labyrinthe = malloc(sizeof(int*)*q);
+    for(int i=0;i<len;i++){
+        lab[i] = calloc(sizeof(int)*p)
+    }
+    return ;    
+}
+
+bool is_real(lab ){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
