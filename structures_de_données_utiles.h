07a608c22cc2976d11359e0d9984a8f13691abc7
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-30 12:22:15 +0200

    Update structures_de_données_utiles.h

--- structures_de_données_utiles.h
+++ structures_de_données_utiles.h
@@ -1,1 +1,10 @@
+typedef struct file {int len; int* data; int len_max;} File;
+typedef struct pile {int len; int* data; int len_max;} Pile;
 
+File* init_file(int len_max);
+void enfiler(File* file,int e);
+int defiler(File* file);
+
+Pile* init_pile(int len_max);
+void enpiler(Pile* pile; int e);
+int depiler(Pile* pile);
