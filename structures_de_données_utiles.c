f79843579a8e94acd81fb16573f7790844c10368
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-03-30 12:06:06 +0200

    Create structures_de_données_utiles.c

---
+++ structures_de_données_utiles.c
@@ -0,0 +1,6 @@
+typedef struct file {int len; int* data; int len_max;} File;
+typedef struct pile {int len; int* data; int len_max;} Pile;
+
+File* init_file(int len_max){
+  /*INitialise une file à zéro*/
+}
