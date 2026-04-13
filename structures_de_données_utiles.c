a73d7347e4a6786111b705889663a3d3ee21b7c3
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-04-13 11:55:53 +0200

    Update structures_de_données_utiles.c

--- structures_de_données_utiles.c
+++ structures_de_données_utiles.c
@@ -1,8 +1,23 @@
+#include <stdbool.h>
+
 typedef struct file {int len; int* data; int len_max;} File;
 typedef struct pile {int len; int* data; int len_max;} Pile;
 
+bool is_empty_F(File* F){
+  return F->len == 0;
+}
+
+bool is_full_F(File* F){
+  return F->len == F->len_max;
+}
+
 File* init_file(int len_max){
   /*INitialise une file à zéro*/
+  File* file = malloc(sizeof(File));
+  file->len = 0;
+  file->data = calloc(sizeof(int)*len_max);
+  file->len_max = len_max;
+  return file;
 }
 
 void enfiler(File F, int elt){
