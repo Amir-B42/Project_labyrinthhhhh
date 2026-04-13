e4b23e8cfc74c0d3eb0a2544009bb4a396642bbc
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-04-13 13:59:04 +0200

    Update lab_graphes.c

--- lab_graphes.c
+++ lab_graphes.c
@@ -2,19 +2,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include "structures_de_données_utiles.h"
 
 //idées : création via DFS -> tt est mur, on casse un chemin
 
-typedef struct arrete {int fleche; int source;} arrete
-typedef struct lab {int* cases; int* arrete} lab;
+typedef struct arrete {int source; int fleche;} arrete;  
+typedef struct lab {int* cases; int* arrete; int len} lab;
 
 lab create(int p, int q){
     return ;    
 }
 
-bool is_real(lab ){
+bool is_real(lab laby, int start, int end){
     
 }
+
+lab* parties_connexes(lab labyrinthe,  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
