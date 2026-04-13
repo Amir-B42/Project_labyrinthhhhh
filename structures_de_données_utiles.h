1616b5264f64a384afbf936b05d1af523915d9c6
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-04-13 11:52:03 +0200

    Update structures_de_données_utiles.h

--- structures_de_données_utiles.h
+++ structures_de_données_utiles.h
@@ -1,10 +1,17 @@
+#include <stdbool.h>
+
 typedef struct file {int len; int* data; int len_max;} File;
 typedef struct pile {int len; int* data; int len_max;} Pile;
 
 File* init_file(int len_max);
 void enfiler(File* file,int e);
 int defiler(File* file);
+bool is_full(File* file);
+bool is_empty(File* file);
 
 Pile* init_pile(int len_max);
 void empiler(Pile* pile; int e);
 int depiler(Pile* pile);
+bool is_full(Pile* pile);
+bool is_empty(Pile* pile);
+
