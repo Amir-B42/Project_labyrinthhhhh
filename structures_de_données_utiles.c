2c29af3fafc70a4702dbe74745fb7126b14dff81
Amir-B42 <147371415+Amir-B42@users.noreply.github.com>
2026-04-13 11:50:21 +0200

    Update structures_de_données_utiles.c

--- structures_de_données_utiles.c
+++ structures_de_données_utiles.c
@@ -4,3 +4,12 @@ typedef struct pile {int len; int* data; int len_max;} Pile;
 File* init_file(int len_max){
   /*INitialise une file à zéro*/
 }
+
+void enfiler(File F, int elt){
+  
+}
+
+int defiler(File F){
+  
+}
+
